4ed5798ecc2ddefc084d8c3afbf37a2856d45f63
Glitch Hub <kbtug22602@kbtcoe.org>
2024-04-26 13:52:01 +0530

    New Changes

---
+++ Assignmnet-1/1-A/Aquarius/assets/js/main.js
@@ -0,0 +1,55 @@
+
+(function() {
+  "use strict";
+
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = document.querySelector('.back-to-top');
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+})()
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+
+      /**
+   * Preloader Remove
+   */
+  const RemovePreloader = () => {
+    let preloader = document.getElementById('preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+          preloader.remove();
+      });
+    }
+  }
+  RemovePreloader();
